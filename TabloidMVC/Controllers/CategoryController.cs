c5ca4f908de73bf319b963ae13c89370486d5576
Autumn Fotopoulos <acarte94@gmail.com>
2021-06-28 16:02:50 -0500

    can create a new category

--- TabloidMVC/Controllers/CategoryController.cs
+++ TabloidMVC/Controllers/CategoryController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TabloidMVC.Models;
 using TabloidMVC.Repositories;
 
 namespace TabloidMVC.Controllers
@@ -23,5 +25,39 @@ namespace TabloidMVC.Controllers
             var categories = _categoryRepository.GetAll();
             return View(categories);
         }
+
+        // GET: categoryController/Details/5
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //GET category create
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST category create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        
+        {
+            try
+            {
+                _categoryRepository.AddCategory(category);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(category);
+            }
+        }
+
+          
     }
 }
--- TabloidMVC/Repositories/CategoryRepository.cs
+++ TabloidMVC/Repositories/CategoryRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using TabloidMVC.Models;
 
@@ -7,6 +8,7 @@ namespace TabloidMVC.Repositories
     public class CategoryRepository : BaseRepository, ICategoryRepository
     {
         public CategoryRepository(IConfiguration config) : base(config) { }
+
         public List<Category> GetAll()
         {
             using (var conn = Connection)
@@ -24,19 +26,44 @@ namespace TabloidMVC.Repositories
 
                     while (reader.Read())
                     {
-                        categories.Add(new Category()
+                        Category category = new Category
                         {
                             Id = reader.GetInt32(reader.GetOrdinal("Id")),
                             Name = reader.GetString(reader.GetOrdinal("name")),
-                        });
+                        };
+                    categories.Add(category);
                     }
-
 
                     reader.Close();
 
                     return categories;
                 }
             }
+        }
+
+        public void AddCategory(Category category)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                //open connection
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    INSERT INTO Category([Name])
+                    OUTPUT INSERTED.ID
+                    VALUES (@name);
+                    ";
+
+                    cmd.Parameters.AddWithValue("@name", category.Name);
+                    // "OUTPUT INSERTED.ID" tells program to create id // AddWithValue adds object to the database and tag.Name tells which column and row
+
+                    int newlyCreatedId = (int)cmd.ExecuteScalar();
+                    category.Id = newlyCreatedId;
+                }
+            }
+
         }
     }
 }
