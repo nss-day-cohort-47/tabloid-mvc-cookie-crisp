3f07740cf8ffb8dd56cc416d6aa074a1af621f04
Cody_Jones <cbjones93@gmail.com>
2021-06-28 14:10:13 -0500

    can view comments

---
+++ TabloidMVC/Controllers/CommentController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+using TabloidMVC.Repositories;
+using Microsoft.VisualBasic;
+
+namespace TabloidMVC.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly IPostRepository _postRepo;
+        private readonly ICommentRepository _commentRepo;
+
+        public CommentController(IPostRepository postRepository, ICommentRepository commentRepository)
+        {
+            _postRepo = postRepository;
+            _commentRepo = commentRepository;
+        }
+        // GET: CommentController
+
+        public ActionResult Index(int id)
+        {
+            var comments = _commentRepo.GetAllCommentsByPostId(id);
+            return View(comments);
+        }
+
+        // GET: CommentController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CommentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CommentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CommentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CommentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CommentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CommentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
